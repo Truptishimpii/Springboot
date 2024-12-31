63fa2d55b2db6bdcd9850e53b3ddece7897bc100
Truptishimpii <163382610+Truptishimpii@users.noreply.github.com>
2024-12-31 07:37:30 +0530

    Create (array)app.java

---
+++ Src/main/java/cwpc.trupti/(array)app.java
@@ -0,0 +1,5 @@
+  String city[] = {"Mumbai", "Delhi", "Pune", "surat", "jaipur"};
+        int city_size = city.length;
+        
+        array ct = new array(city_size,city);
+        ct.get_all_city();
